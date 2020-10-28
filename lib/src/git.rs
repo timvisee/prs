6db735bc587ba2543fa00acd3377a63ff890ffbb
timvisee <tim@visee.me>
2020-10-28 12:16:15 +0100

    Improve git handling through git CLI, minimize number of subprocesses

--- lib/src/git.rs
+++ lib/src/git.rs
@@ -1,5 +1,7 @@
+use std::env;
+use std::ffi::OsStr;
 use std::path::Path;
-use std::process::{Command, Output};
+use std::process::{Command, ExitStatus};
 use std::time::SystemTime;
 
 use anyhow::Result;
@@ -8,33 +10,46 @@ use thiserror::Error;
 /// The git FETCH_HEAD file.
 const GIT_FETCH_HEAD_FILE: &str = ".git/FETCH_HEAD";
 
+/// Environment variable git uses to modify the ssh command.
+#[cfg(not(windows))]
+const GIT_ENV_SSH: &str = "GIT_SSH_COMMAND";
+
+/// Custom ssh command for git.
+///
+/// With this custom SSH command we enable SSH session reuse, to make remote git operations much
+/// quicker for repositories using an SSH URL. This greatly improves prs sync speeds.
+// TODO: make configurable, add current user ID to path
+#[cfg(not(windows))]
+const GIT_ENV_SSH_CMD: &str = "ssh -o 'ControlMaster auto' -o 'ControlPath /tmp/.prs-session--%r@%h:%p' -o 'ControlPersist 1h'";
+
 /// Invoke git pull.
 pub fn git_pull(repo: &Path) -> Result<()> {
-    git(repo, "pull -q")
+    git(repo, &["pull", "-q"])
 }
 
 /// Check if repository has remote configured.
 pub fn git_has_remote(repo: &Path) -> Result<bool> {
-    let output = git_output(repo, "remote")?;
-    let stdout = String::from_utf8(output.stdout)?;
-    Ok(!stdout.trim().is_empty())
+    Ok(!git_stdout(repo, &["remote"])?.is_empty())
 }
 
 /// Get the current git branch name.
 pub fn git_current_branch(repo: &Path) -> Result<String> {
-    let output = git_output(repo, "rev-parse --abbrev-ref HEAD")?;
-    let branch = std::str::from_utf8(&output.stdout)?.trim();
-    assert!(!branch.is_empty(), "git returned invalid branch name");
+    let branch = git_stdout(repo, &["rev-parse", "--abbrev-ref", "HEAD"])?;
+    assert!(!branch.is_empty(), "git returned empty branch name");
+    assert!(!branch.contains("\n"), "git returned multiple branches");
     Ok(branch.into())
 }
 
 /// Get upstream branch for given branch if there is any.
 pub fn git_branch_upstream<S: AsRef<str>>(repo: &Path, reference: S) -> Result<Option<String>> {
-    let output = git_output(
+    let upstream = git_stdout(
         repo,
-        format!("rev-parse --abbrev-ref {}@{{upstream}}", reference.as_ref()),
+        &[
+            "rev-parse",
+            "--abbrev-ref",
+            &format!("{}@{{upstream}}", reference.as_ref()),
+        ],
     )?;
-    let upstream = std::str::from_utf8(&output.stdout)?.trim();
     if upstream.is_empty() {
         return Ok(None);
     }
@@ -47,8 +62,7 @@ pub fn git_branch_upstream<S: AsRef<str>>(repo: &Path, reference: S) -> Result<O
 
 /// Get the hash of a reference.
 pub fn git_ref_hash<S: AsRef<str>>(repo: &Path, reference: S) -> Result<String> {
-    let output = git_output(repo, format!("rev-parse {}", reference.as_ref()))?;
-    let hash = std::str::from_utf8(&output.stdout)?.trim();
+    let hash = git_stdout(repo, &["rev-parse", reference.as_ref()])?;
     assert_eq!(hash.len(), 40, "git returned invalid hash");
     Ok(hash.into())
 }
@@ -59,67 +73,68 @@ pub fn git_last_pull_time(repo: &Path) -> Result<SystemTime> {
     Ok(repo
         .join(GIT_FETCH_HEAD_FILE)
         .metadata()
-        .map_err(Err::Other)?
-        .modified()
+        .and_then(|m| m.modified())
         .map_err(Err::Other)?)
 }
 
-/// Invoke a git command.
-fn git<S: AsRef<str>>(repo: &Path, cmd: S) -> Result<()> {
-    system(
-        format!("git -C {:?} {}", repo.display(), cmd.as_ref()),
-        Some(&repo),
-    )
-    .map_err(|err| Err::GitCli(err).into())
+/// Invoke a git command with the given arguments.
+///
+/// The command will take over the user console for in/output.
+fn git<I, S>(repo: &Path, args: I) -> Result<()>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    cmd_assert_status(cmd_git(args, Some(repo)).status().map_err(Err::System)?)
 }
 
-/// Invoke a git command.
-fn git_output<S: AsRef<str>>(repo: &Path, cmd: S) -> Result<Output> {
-    system_output(
-        format!("git -C {:?} {}", repo.display(), cmd.as_ref()),
-        Some(&repo),
-    )
-    .map_err(|err| Err::GitCli(err).into())
+/// Invoke a git command with the given arguments, return stdout.
+fn git_stdout<I, S>(repo: &Path, args: I) -> Result<String>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let output = cmd_git(args, Some(repo)).output().map_err(Err::System)?;
+    cmd_assert_status(output.status)?;
+
+    Ok(std::str::from_utf8(&output.stdout)
+        .map_err(|err| Err::GitCli(err.into()))?
+        .trim()
+        .into())
 }
 
-/// Invoke the given system command.
-// TODO: do not create two subprocesses here (sh & git)
-fn system(cmd: String, dir: Option<&Path>) -> Result<()> {
-    // Invoke command
-    // TODO: make this compatible with Windows
-    let mut process = Command::new("sh");
-    process.arg("-c").arg(&cmd);
+/// Build a git command to run.
+fn cmd_git<I, S>(args: I, dir: Option<&Path>) -> Command
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut cmd = Command::new("git");
+
     if let Some(dir) = dir {
-        process.current_dir(dir);
+        cmd.arg("-C");
+        cmd.arg(dir);
+        cmd.current_dir(dir);
     }
-    let status = process.status().map_err(Err::System)?;
 
-    // Report status errors
+    // Set custom git ssh command to speed up remote operations
+    #[cfg(not(windows))]
+    if env::var_os(GIT_ENV_SSH).is_none() {
+        cmd.env(GIT_ENV_SSH, GIT_ENV_SSH_CMD);
+    }
+
+    cmd.args(args);
+    cmd
+}
+
+/// Assert the exit status of a command.
+///
+/// Returns error is status is not succesful.
+fn cmd_assert_status(status: ExitStatus) -> Result<()> {
     if !status.success() {
         return Err(Err::Status(status).into());
     }
-
     Ok(())
-}
-
-/// Invoke the given system command.
-// TODO: do not create two subprocesses here (sh & git)
-fn system_output(cmd: String, dir: Option<&Path>) -> Result<Output> {
-    // Invoke command
-    // TODO: make this compatible with Windows
-    let mut process = Command::new("sh");
-    process.arg("-c").arg(&cmd);
-    if let Some(dir) = dir {
-        process.current_dir(dir);
-    }
-    let output = process.output().map_err(Err::System)?;
-
-    // Report status errors
-    if !output.status.success() {
-        return Err(Err::Status(output.status).into());
-    }
-
-    Ok(output)
 }
 
 #[derive(Debug, Error)]

1cbd411bc63c21afe8919da13cabc0b6c52e9b67
timvisee <tim@visee.me>
2021-06-24 01:52:08 +0200

    Fix compilation error on MSRVU

--- cli/src/action/housekeeping/run.rs
+++ cli/src/action/housekeeping/run.rs
@@ -117,7 +117,7 @@ fn set_git_ignore(store: &Store) -> Result<(), std::io::Error> {
     file.read_to_string(&mut contents)?;
 
     // Add each entry if it doesn't exist
-    for entry in ENTRIES {
+    for entry in &ENTRIES {
         if !contents.lines().any(|l| l.trim() == entry) {
             file.write_all(LINE_ENDING.as_bytes())?;
             file.write_all(entry.as_bytes())?;

c37bb89523b411a37d3c7fda825efa693b8e5e8c
timvisee <tim@visee.me>
2021-06-29 00:40:33 +0200

    Add tomb lock as alias for tomb open

--- cli/src/cmd/subcmd/tomb/open.rs
+++ cli/src/cmd/subcmd/tomb/open.rs
@@ -5,13 +5,17 @@ pub struct CmdOpen;
 
 impl CmdOpen {
     pub fn build<'a>() -> App<'a> {
-        App::new("open").alias("o").about("Open tomb").arg(
-            Arg::new("timer")
-                .long("timer")
-                .short('t')
-                .alias("time")
-                .value_name("TIME")
-                .about("Time after which to close the Tomb"),
-        )
+        App::new("open")
+            .alias("o")
+            .alias("lock")
+            .about("Open tomb")
+            .arg(
+                Arg::new("timer")
+                    .long("timer")
+                    .short('t')
+                    .alias("time")
+                    .value_name("TIME")
+                    .about("Time after which to close the Tomb"),
+            )
     }
 }

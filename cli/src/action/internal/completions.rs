07f17a93d19fb6bc92c9c7f3ae3f5cc750b1f973
Stefan Junker <mail@stefanjunker.de>
2024-06-22 21:29:15 +0200

    completions/zsh: provide list to query

--- cli/src/action/internal/completions.rs
+++ cli/src/action/internal/completions.rs
@@ -90,11 +90,25 @@ where
             buf.write_fmt(format_args!("{inner_buf}"))
                 .expect("failed to write to generated file"); // Same panic that clap_complete would trigger
         }
-        // TODO: patch other completion scripts to complete secret names like with bash
+        Shell::Zsh => {
+            let mut inner_buf = Vec::new();
+            clap_complete::generate(shells::Zsh, app, bin_name, &mut inner_buf);
+
+            // Patch bash completion to complete secret names
+            let inner_buf = String::from_utf8(inner_buf)
+                .expect("clap_complete::generate should always return valid utf-8")
+                .replace(
+                    ":QUERY -- Secret query:",
+                    ":QUERY -- Secret query:($(prs list --list --quiet))",
+                );
+
+            buf.write_fmt(format_args!("{inner_buf}"))
+                .expect("failed to write to generated file"); // Same panic that clap_complete would trigger
+        }
+        // TODO: patch other completion scripts to complete secret names
         Shell::Elvish => clap_complete::generate(shells::Elvish, app, bin_name, buf),
         Shell::Fish => clap_complete::generate(shells::Fish, app, bin_name, buf),
         Shell::PowerShell => clap_complete::generate(shells::PowerShell, app, bin_name, buf),
-        Shell::Zsh => clap_complete::generate(shells::Zsh, app, bin_name, buf),
     }
 }
 
